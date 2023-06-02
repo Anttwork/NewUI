8edb8a1cd40f1e343be7b1cace420b63f201e58f
Anttwork <345183106@qq.com>
2023-06-02 11:37:26 +0800

    添加主页比赛适配器

---
+++ app/src/main/java/com/ui/uiapplication/ui/adapter/HomematchAdapter.java
@@ -0,0 +1,34 @@
+package com.ui.uiapplication.ui.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+//主页_比赛适配
+class HomematchAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public HomematchAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
